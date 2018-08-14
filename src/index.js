1cde015260ec4ab430e4caa92db887b72fc6ce2e
Oguzhan Inan <oguzhan3488@gmail.com>
2018-08-14 17:53:10 +0300

    change name build to lib

--- src/index.js
+++ src/index.js
@@ -5,6 +5,36 @@ import gm from 'gm'
 gm.subClass({imageMagick: true});
 
 
+gm('/home/oguzhan/Pictures/foto.jpg')
+    // .blur(10,5)
+    // .chop(500, 500, 100, 10)
+    .borderColor('#444')
+    .border(20, 20)
+    // .charcoal(1)
+    // .colorize(12, 50, 60)
+    // .colors(100)
+    // .crop(600, 300, 20, 20)
+    // .flip()
+    // .flop()
+    // .frame(20, 20, 10, 10)
+    // .gamma(12,1,35)
+    // .geometry(600, 500, '%')
+    // .gravity('Center')
+    // .highlightColor('red')
+    // .fuzz()
+    // .gaussian(12)
+    // .channel('Opacity')
+    // .append('/home/oguzhan/Pictures/icon.png', true)
+    // .watermark(23, 32)
+    .stroke('red', 25)
+    .fill('yellow')
+    .drawRectangle(10, 20, 200, 200, )
+    .fontSize(64)
+    .fill('red')
+    .drawText(20, 30, 'TEXT', 'CENTER')
+    .write('/home/oguzhan/Pictures/foto2.jpg', err => console.log(err))
+
+process.exit()
 
 inquirer.prompt([Questions.input_type])
     .then(({input_type}) => {
